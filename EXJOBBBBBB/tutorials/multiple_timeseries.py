602ab41f5c5447dae4d3f5ed7e2d462c4f0ebd0c
Carina Wickström <49478094+carinawic@users.noreply.github.com>
2022-02-14 16:46:14 +0100

    progress

---
+++ EXJOBBBBBB/tutorials/multiple_timeseries.py
@@ -0,0 +1,9 @@
+from tbats import TBATS, BATS
+
+import "github.com/intive-DataScience/tbats"
+
+# Fit the model
+estimator = TBATS(seasonal_periods=(7, 365.25))
+model = estimator.fit(y_to_train)
+# Forecast 365 days ahead
+y_forecast = model.forecast(steps=365)
