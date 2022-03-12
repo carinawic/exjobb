7fe3be956226af79c2109d94a446df21ab40e18f
Carina Wickström <49478094+carinawic@users.noreply.github.com>
2022-03-12 18:18:45 +0100

    working now

--- EXJOBBBBBB/tutorials/ARIMA_FOURIER/sarima_fourier.py
+++ EXJOBBBBBB/tutorials/ARIMA_FOURIER/sarima_fourier.py
@@ -18,6 +18,7 @@ from numpy import polyfit
 # https://www.quora.com/How-does-multivariate-ARIMA-work
 
 
+
 def working():
     #df = pd.read_csv('kaggle_sales.csv')
     #df = df[(df['store'] == 1) & (df['item'] == 1)] # item 1 in store 1
@@ -37,19 +38,35 @@ def working():
 
     y = df['clicks_out'][mask_clickouts]
 
-
+    #renamed = df.rename(columns={'clicks_out': 'decomposition of data'})
+    #y = renamed['decomposition of data'][mask_clickouts]
     
     y_to_train = y.iloc[:(len(y)-365)]
     y_to_test = y.iloc[(len(y)-365):] # last year for testing
 
-    #a = np.zeros(len(y))
 
-    #for n in range(len(y)):
-    #    a[n]=15*np.sin(np.deg2rad(n-90))+20
-
-
+    
+    # creating the curve polnomial
+    X = [i%365 for i in range(0, len(y.values))]
+    y_vals = y.values
+    degree = 8
+    coef = polyfit(X, y_vals, degree)
+    print('Coefficients: %s' % coef)
+    # create curve
+    curve = list()
+    for i in range(len(X)):
+        value = coef[-1]
+        for d in range(degree):
+            value += X[i]**(degree-d) * coef[d]
+        curve.append(value)
+    
     #plt.plot( range(len(y_to_train)), y_to_train, color='green')
-    #plt.plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_to_test, color='blue')
+    #plt.plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_to_test, color='green')
+    #plt.plot( range(len(curve)), curve, color='black', linewidth=3)
+       
+        
+
+    
 
     #plt.show()
 
@@ -68,18 +85,6 @@ def working():
 
     #print(arima_exog_model.summary())
    
-    X = [i%365 for i in range(0, len(y.values))]
-    y = y.values
-    degree = 4
-    coef = polyfit(X, y, degree)
-    print('Coefficients: %s' % coef)
-    # create curve
-    curve = list()
-    for i in range(len(X)):
-        value = coef[-1]
-        for d in range(degree):
-            value += X[i]**(degree-d) * coef[d]
-        curve.append(value)
     # plot curve over original data
   
     
@@ -89,25 +94,83 @@ def working():
     #exog = exog.set_index(pd.PeriodIndex(exog['date'], freq='D'))
     #exog['passengers'] = y
 
+    
+    
     decompose_result = seasonal_decompose(y, model="additive", period=365)
     trend = decompose_result.trend
     seasonal = decompose_result.seasonal
     residual = decompose_result.resid
-
-
-
+    #decompose_result.plot()
     #plt.plot( range(len(trend)), trend, color='purple')
-    
-    plt.plot( range(len(trend)), trend, color='black')
+    trend = [x for x in trend if not math.isnan(x)] # remove nan values, then linear approx is a bit meh :)
+    #plt.plot( range(len(trend)), trend, color='black')
     #plt.plot( range(len(seasonal)), seasonal, color='green')
     #plt.plot( range(365,365*2),seasonal[:365], color='blue')
+    m,b = np.polyfit(range(len(trend)),trend,1)
+    #plt.plot(range(len(trend)),m*range(len(trend))+b, color='purple')
+    #plt.plot(range(len(trend)),m*np.log(range(len(trend)))+b, color='green')
+    
+    clickouts = np.array(y.values)
 
-    m,b = np.polyfit(range(trend),trend,1)
-    plt.plot(range(trend),m*range(trend)+b,'-', color='purple')
+    clickouts_wo_trend = []
+    clickouts_wo_trend_or_seasonality = []
+    moving_average = []
+
+    
+    seasonal = [x for x in seasonal if not x is(None)] # remove None values
+
+    print("seasonal")
+    print(len(seasonal))
+    print("clickouts")
+    print(len(clickouts))
+
+    # first remove trend, then remove seasonality!! because otherwise the seasonality is skewed and tricky
+
     
 
+    for i,real_val in enumerate(clickouts):
+        #print(real_val)
+        #print((m*i+b))
+        #print(seasonal[i])
+        #preprocessed_clickouts.append(real_val - (m*i+b) - seasonal[i])
+        
+
+        clickouts_wo_trend.append(real_val - (m*i)) # centered around b now!
+        clickouts_wo_trend_or_seasonality.append(clickouts_wo_trend[i] - curve[i] + b)
+        #moving_average.append((m*i+b) + curve[i])
+
+    """
+    # plotting data without seasonality or trend!
+    fig, axs = plt.subplots(3)
+    fig.suptitle('Removing trend and seasonality')
+    axs[0].plot( range(len(y_to_train)), y_to_train, color='green')
+    axs[0].plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_to_test, color='green')
+    axs[0].set_title('original flight data')
+    axs[1].plot(range(len(clickouts_wo_trend)), clickouts_wo_trend, color='black')
+    axs[1].set_title('data without trend')
+    axs[2].plot(range(len(clickouts_wo_trend_or_seasonality)), clickouts_wo_trend_or_seasonality, color='purple')
+    axs[2].set_title('data without seasonality')
+    fig.tight_layout()
+
+    for ax in axs.flat:
+        ax.set(xlabel='days', ylabel='clicks')
+
+    
+    """
+    
+    
+   
+
     #plt.plot( range(len(y_to_train)), y_to_train, color='green')
-    #plt.plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_to_test, color='blue')
+    #plt.plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_to_test, color='green')
+    #plt.plot( range(len(clickouts_wo_trend)), clickouts_wo_trend, color='black')
+    #plt.plot( range(len(clickouts_wo_trend_or_seasonality)), clickouts_wo_trend_or_seasonality, color='orange')
+
+    #plt.plot(range(len(preprocessed_clickouts)), preprocessed_clickouts, color='green')
+
+    #plt.plot( range(len(y_to_train)), y_to_train, color='green')
+    #plt.plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_to_test, color='green')
+    #plt.plot(range(len(curve)), curve, color='red')
     #plt.plot( range(len(y_to_train), len(y_to_train)+len(y_to_test)), y_arima_exog_forecast, color="red")
     
     #pyplot.plot(curve, color='black', linewidth=3)
